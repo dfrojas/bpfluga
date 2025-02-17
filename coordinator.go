fa9dad50d680208b97ea0c738541444c06060b5b
Diego Fernando Rojas <rojastorrado@gmail.com>
2025-02-17 09:49:03 -0500

    Config from YAML file
    This commit adds support to config the eBPF deployment via declarative through a YAML file.

--- coordinator.go
+++ coordinator.go
@@ -2,28 +2,84 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/yaml.v3"
 	"log"
 	"os"
 	"os/exec"
 )
 
+type HostConfig struct {
+	Name           string `yaml:"name"`
+	Address        string `yaml:"address"`
+	Port           int    `yaml:"port"`
+	User           string `yaml:"user"`
+	PrivateKeyPath string `yaml:"privateKeyPath"`
+}
+
+type EBPFConfig struct {
+	SourceType   string `yaml:"sourceType"`
+	FilePath     string `yaml:"file_path"`
+	CompileFlags string `yaml:"compileFlags"`
+}
+
+type MetricsConfig struct {
+	Name        string `yaml:"name"`
+	Description string `yaml:"description"`
+}
+
+type DeployConfig struct {
+	Hosts   []HostConfig    `yaml:"hosts"`
+	EBPF    EBPFConfig      `yaml:"ebpf"`
+	Metrics []MetricsConfig `yaml:"metrics"`
+}
+
+func loadConfig(filename string) (*DeployConfig, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read config file: %v", err)
+	}
+
+	var deployConfig DeployConfig
+	err = yaml.Unmarshal(data, &deployConfig)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to unmarshal config: %v", err)
+	}
+
+	return &deployConfig, nil
+}
+
 func main() {
-	host := os.Args[1]
-	loaderBin := os.Args[2]
-	ebpfObj := os.Args[3]
-	keyPath := os.Args[4]
+
+	deployConfigPath := os.Args[1]
+
+	deployConfig, err := loadConfig(deployConfigPath)
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	host := deployConfig.Hosts[0]
+
+	address := host.Address
+	user := host.User
+
+	userAndAddress := fmt.Sprintf("%s@%s", user, address)
+
+	keyPath := host.PrivateKeyPath
+	// loaderBin := deployConfig.EBPF.FilePath -> Coordinador debería compilar el loader?
+	loaderBin := "./loader/loader"
+	ebpfObj := "./bpf/minimal.o" // TODO: compilar el ebpfObj?
 
 	remoteLoader := "/tmp/loader"
 	remoteObj := "/tmp/minimal.o"
 
 	// 1. Copy the loader binary to the remote machine
-	scpCmd1 := exec.Command("scp", "-i", keyPath, loaderBin, fmt.Sprintf("%s:%s", host, remoteLoader))
+	scpCmd1 := exec.Command("scp", "-i", keyPath, loaderBin, fmt.Sprintf("%s:%s", userAndAddress, remoteLoader))
 	if out, err := scpCmd1.CombinedOutput(); err != nil {
 		log.Fatalf("Failed to SCP loader: %v\nOutput: %s", err, out)
 	}
 
 	// 2. Copy the minimal.o eBPF object
-	scpCmd2 := exec.Command("scp", "-i", keyPath, ebpfObj, fmt.Sprintf("%s:%s", host, remoteObj))
+	scpCmd2 := exec.Command("scp", "-i", keyPath, ebpfObj, fmt.Sprintf("%s:%s", userAndAddress, remoteObj))
 	if out, err := scpCmd2.CombinedOutput(); err != nil {
 		log.Fatalf("Failed to SCP eBPF .o file: %v\nOutput: %s", err, out)
 	}
@@ -33,7 +89,7 @@ func main() {
 
 	// SSH COMMAND
 	remoteCmd := fmt.Sprintf("'%s'", runCmd)
-	sshCmd := exec.Command("ssh", "-i", keyPath, host, "bash", "-c", remoteCmd)
+	sshCmd := exec.Command("ssh", "-i", keyPath, userAndAddress, "bash", "-c", remoteCmd)
 
 	out, err := sshCmd.CombinedOutput()
 	if err != nil {
--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module ebpf-test
 
 go 1.24.0
 
+require gopkg.in/yaml.v3 v3.0.1
+
 require (
 	github.com/cilium/ebpf v0.17.3 // indirect
 	golang.org/x/sys v0.30.0 // indirect
